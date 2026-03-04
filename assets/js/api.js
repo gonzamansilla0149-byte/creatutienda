31ec76d08eff753054ab9c66991c41edda7c5bd9
gonzamansilla0149-byte <gonzamansilla0149@gmail.com>
2026-03-04 11:36:28 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,1 +1,17 @@
+const API_BASE = "creatutienda.gonzamansilla0149.workers.dev/api"
 
+export async function api(path, options = {}) {
+
+    const res = await fetch(`${API_BASE}${path}`, {
+        headers: {
+            "Content-Type": "application/json"
+        },
+        ...options
+    })
+
+    if (!res.ok) {
+        throw new Error("API error")
+    }
+
+    return res.json()
+}
