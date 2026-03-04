b02f737398f805b12f1fd67c29aa76b36a89e049
gonzamansilla0149-byte <gonzamansilla0149@gmail.com>
2026-03-04 11:37:03 -0300

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,1 +1,26 @@
+import { api } from "./api.js"
 
+
+export async function register(email) {
+
+    const data = await api("/platform/user", {
+        method: "POST",
+        body: JSON.stringify({ email })
+    })
+
+    localStorage.setItem("userId", data.userId)
+
+    return data
+}
+
+
+
+export function getSession() {
+    return localStorage.getItem("userId")
+}
+
+
+
+export function logout() {
+    localStorage.removeItem("userId")
+}
