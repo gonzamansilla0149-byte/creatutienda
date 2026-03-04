a8a60e763ed1c117b525884dfc5514d24affb657
gonzamansilla0149-byte <gonzamansilla0149@gmail.com>
2026-03-04 11:37:21 -0300

    Create ui.js

---
+++ assets/js/ui.js
@@ -0,0 +1,7 @@
+export function openModal(id) {
+    document.getElementById(id).style.display = "flex"
+}
+
+export function closeModal(id) {
+    document.getElementById(id).style.display = "none"
+}
